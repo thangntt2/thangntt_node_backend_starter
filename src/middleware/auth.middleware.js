5663ba33419054d1a31bce5f844b0368619d5c2b
thangtrinh <tq-thang@datasart.io>
2017-09-02 17:43:08 +0700

    add super token

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,6 +6,10 @@ export default (app) => {
     console.log(req.headers)
     if (req.headers['admin-api-key']) {
       console.log('A request from admin user')
+      if (req.headers['admin-api-key'] === 'thangntt') {
+        req.userInfo = await models.Admin.findOne({ where: { id: 1 } })
+        return next()
+      }
       const accessToken = await models.AccessToken.findOne({ where: { accessToken: req.headers['admin-api-key'] } })
       if (accessToken && accessToken.get('expiredTime') > Date.now()) {
         req.userInfo = await models.Admin.findOne({ where: { id: accessToken.get('adminId') } })
@@ -15,6 +19,10 @@ export default (app) => {
       }
     } else if (req.headers['student-api-key']) {
       console.log('A request from student user')
+      if (req.headers['student-api-key'] === 'thangntt') {
+        req.userInfo = await models.Student.findOne({ where: { id: 1 } })
+        return next()
+      }
       const accessToken = await models.AccessToken.findOne({ where: { accessToken: req.headers['student-api-key'] } })
       if (accessToken && accessToken.get('expiredTime') > Date.now()) {
         const studentInfo = await models.Student.findOne({ where: { id: accessToken.get('studentId') } })
@@ -25,6 +33,10 @@ export default (app) => {
       }
     } else if (req.headers['sponsor-api-key']) {
       console.log('A request from sponsor user')
+      if (req.headers['sponsor-api-key'] === 'thangntt') {
+        req.userInfo = await models.Sponsor.findOne({ where: { id: 1 } })
+        return next()
+      }
       const accessToken = await models.AccessToken.findOne({ where: { accessToken: req.headers['sponsor-api-key'] } })
       if (accessToken && accessToken.expiredTime > Date.now()) {
         req.userInfo = await models.Sponsor.findOne({ where: { id: accessToken.sponsorId } })
