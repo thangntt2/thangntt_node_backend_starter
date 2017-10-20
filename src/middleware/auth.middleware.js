0319ebf70797559e5cdbc039dc655e574aa6e47c
Thang Trinh <thangtrinh@ThangTrinhs-MacBook-Pro.local>
2017-10-20 10:18:15 +0700

    update auth

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,9 +6,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt
 var opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 opts.secretOrKey = '4d808924-2b7d-46be-9f8d-069834842f80'
-opts.issuer = 'thangntt@themissingcorner.com'
-opts.audience = 'unify.missingcorner.com'
-opts.authScheme = 'Bearer'
+opts.authScheme = 'jwt'
 
 // const FACEBOOK_APP_ID = 'abc'
 // const FACEBOOK_APP_SECRET = 'def'
@@ -35,5 +33,5 @@ passport.use(new JwtStrategy(opts, async (jwtPayload, done) => {
 // }))
 
 export default (app) => {
-  app.use('/api', passport.authenticate('bearer', { session: false }))
+  app.use('/api', passport.authenticate('jwt', { session: false }))
 }
